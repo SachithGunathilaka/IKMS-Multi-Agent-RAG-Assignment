43c3e91ab972a64c4e7738c7a05e3e2dcfc2d203
SachithGunathilaka <sachithruvinda@gmail.com>
2026-03-21 22:47:50 +0530

    fix: remove react compiler plugin from vite config

--- FrontEnd-IKMS-MultiAgent-RAG/vite.config.js
+++ FrontEnd-IKMS-MultiAgent-RAG/vite.config.js
@@ -3,14 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
+    react(),
     tailwindcss(),
   ],
   resolve: {
