c90949ef67ac506781d6e8e51fd2325070e68572
SachithGunathilaka <sachithruvinda@gmail.com>
2026-03-21 14:29:00 +0530

    add: updated vercel app port

--- BackEnd-IKMS-MultiAgent-RAG/src/app/api.py
+++ BackEnd-IKMS-MultiAgent-RAG/src/app/api.py
@@ -33,7 +33,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",  # Default Vite port
         "http://127.0.0.1:5173",
-        "https://your-vercel-app.vercel.app",
+        "https://ikms-multi-agent-rag-ashy.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
